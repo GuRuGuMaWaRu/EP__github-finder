42258cda27f3f40007a64821a935c5f9e02bd413
GuRuGu <peter.k.nets@gmail.com>
2019-10-23 18:03:11 +0300

    Creat NotFound page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
-import User from "./components/users/User";
 import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
+import User from "./components/users/User";
 
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
@@ -24,6 +25,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
